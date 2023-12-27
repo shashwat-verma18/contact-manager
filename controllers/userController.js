e29d554ed856cf254224404c7c6f5e04e5c25f95
Shashwat Verma <shashwatv18@gmail.com>
2023-12-27 17:54:42 +0530

    project uploaded

---
+++ controllers/userController.js
@@ -0,0 +1,79 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+
+    const userAvailable = await User.findOne({ email });
+
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+    //Hash Passwword
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    console.log(user);
+    if (user) {
+        res.status(201).json({ _id: user.id, username: user.username, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid!");
+    }
+    res.json({ message: "register user" });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id
+                },
+            }, process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "20m"}
+        );
+        res.status(200).json({ accessToken });
+    }else{
+        res.status(401);
+        throw new Error("Incorrect username or password");
+    }
+    res.json({ message: "login user" });
+});
+
+const currentUser = asyncHandler(async (req, res) => {
+
+    res.json(req.user);
+});
+
+module.exports = { registerUser, loginUser, currentUser };
